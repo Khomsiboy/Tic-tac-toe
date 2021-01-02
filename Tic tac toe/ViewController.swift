62e04830cbbc66cd5a099063a1a8f76a286064ac
khomsiboy <domkhomsan789@hotmail.com>
2021-01-02 20:13:52 +0000

    Added buttons and grit

--- Tic tac toe/ViewController.swift
+++ Tic tac toe/ViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var Player = 1
+    var boardRoom = [0,0,0,0,0,0,0,0,0]
+    
+    @IBAction func XOButton(_ sender: UIButton) {
+        
+        if boardRoom[sender.tag-1 ] == 0{
+            if Player == 1{
+                sender.setImage(UIImage(named: "o.png"), for: UIControl.State())
+                Player = 2
+            }
+            else{
+                sender.setImage(UIImage(named: "x.png"), for: UIControl.State())
+                Player = 1
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the 
+       
     }
 
 
